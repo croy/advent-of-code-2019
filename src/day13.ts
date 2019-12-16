45c035f857b3e63855099c0fb73ff220259fcd83
Christian Roy <christian.roy@gmail.com>
2019-12-16 13:49:25 -0800

    day 13 part 2

--- src/day13.ts
+++ src/day13.ts
@@ -1,29 +1,52 @@
-import fs from 'fs';
-import IntCode from './IntCode';
+import fs from "fs";
+import IntCode from "./IntCode";
 
-const program = fs.readFileSync('input/day13', 'utf-8').split(',').map(i => parseInt(i, 10));
+const program = fs
+  .readFileSync("input/day13", "utf-8")
+  .split(",")
+  .map(i => parseInt(i, 10));
 const compy = new IntCode(program);
 
-const stepper = compy.execute([]);
+const input: number[] = [];
+const stepper = compy.execute(input);
 
-let blockCount = 0;
-
+let score = 0;
+let ballX = 0;
+let paddleX = 0;
 while (true) {
-  const x = stepper.next();
-  const y = stepper.next();
-  const id = stepper.next();
+  let x = stepper.next();
   if (x.done) {
+    console.log(`score: ${score}`);
     break;
   }
-  if (x.value === undefined || y.done || id.done || y.value === undefined || id.value === undefined) {
-    throw 'unexpected output';
+  if (x.value === undefined) {
+    //INPUT
+    if (ballX > paddleX) {
+      input.push(1);
+    } else if (ballX < paddleX) {
+      input.push(-1);
+    } else {
+      input.push(0);
+    }
+    x = stepper.next();
   }
-  if (id.value === 3) {
-    console.log(`(${x},${y})`);
+
+  const y = stepper.next();
+  const id = stepper.next();
+  if (
+    x.value === undefined ||
+    y.done ||
+    id.done ||
+    y.value === undefined ||
+    id.value === undefined
+  ) {
+    throw "unexpected output";
   }
-  if (id.value === 2) {
-    blockCount++;
+  if (x.value === -1 && y.value === 0) {
+    score = id.value;
+  } else if (id.value === 3) {
+    paddleX = x.value;
+  } else if (id.value === 4) {
+    ballX = x.value;
   }
 }
-
-console.log(blockCount);
